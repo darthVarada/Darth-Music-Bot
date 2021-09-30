2d17bab0396210bc4d5ecfa7f892f4853d01cfb0
Arthur <arthur@pop-os.localdomain>
2021-09-30 20:48:33 -0300

    substiuicao do config.josn pelo .env, adicoes no index.js, nodemon pelo npm start e substituicao do js 'classico' pelo es6 module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-const Discord = require("discord.js");
+import { MessageEmbed, Client } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const config = require("./data/config.json");
-
-const intents = new Discord.Intents(32767);
-
-const client = new Discord.Client({ intents });
-
-//client.on("ready", () => console.log("he is on!"));
-
-client.on("messageCreate", message => {
-
-    if(message.content == "oi") message.reply("coe mano!");
-    
+const client = new Client({
+    intents: [
+        32767,
+    ]
 });
 
-client.login(config.token);
+client.on("ready", () => console.log("he is on!"));
+
+client.on("messageCreate", msg => {
+    if (!msg.content.includes(process.env.PREFIX)){
+        return;
+    } else {
+        const command = msg.content.substr(1, msg.content.length);
+        if(command == "oi") message.reply("coe mano!");
+        if(command === "ping") msg.reply("pong");
+        if(command === "source") {
+            const embed = new MessageEmbed()
+                .setTitle("Código fonte")
+                .setDescription("Código fonte do aplicativo no github")
+                .setURL("https://github.com/darthVarada/Darth-Music-Bot");
+
+            msg.channel.send({content: "hello", embeds: [embed]});
+        }
+    }
+});
+
+client.login(process.env.TOKEN);
